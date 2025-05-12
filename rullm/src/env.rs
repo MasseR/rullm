4231b4bda0490cd5d1c5b2e34c521b2055407a87
Mats Rauhala <mats.rauhala@iki.fi>
2025-05-12 22:02:47 +0300

    Override conf path

--- rullm/src/env.rs
+++ rullm/src/env.rs
@@ -2,19 +2,17 @@ use crate::{args::Args, conf::Conf, mcp::MCP, openai::OpenAIClient};
 
 pub struct Env {
     pub openai_client: OpenAIClient,
-    pub args: Args,
     pub mcp: MCP,
     pub conf: Conf,
 }
 
 impl Env {
     pub async fn build(args: Args) -> anyhow::Result<Env> {
-        let conf = Conf::build(None)?;
+        let conf = Conf::build(args.conf_file)?;
         let mcp = MCP::build(&conf).await?;
         let openai_client = OpenAIClient::build(&conf, &mcp).await?;
         Ok(Env {
             openai_client,
-            args,
             mcp,
             conf,
         })

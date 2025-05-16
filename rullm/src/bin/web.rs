3ec5d2c4865fdcae0891ee5fa35a062dd52798ae
Mats Rauhala <mats.rauhala@iki.fi>
2025-05-16 23:13:26 +0300

    Objects? There's still a lot of duplication

--- rullm/src/bin/web.rs
+++ rullm/src/bin/web.rs
@@ -1,4 +1,4 @@
-use std::{ops::Deref as _, path::PathBuf, sync::Arc};
+use std::{path::PathBuf, sync::Arc};
 
 use anyhow::{anyhow, bail};
 use async_openai::types::{
@@ -92,11 +92,28 @@ struct HtmxMessage {
     chat_message: String,
 }
 
+
 struct HtmxChat {
     socket: WebSocket,
+    messages: Vec<ChatCompletionRequestMessage>,
+    env: Arc<Env>,
 }
 
 impl HtmxChat {
+    fn new(socket: WebSocket, env: Arc<Env>) -> Self {
+        let mut messages = Vec::new();
+        let system_prompt = format!(
+            "You are a helpful assistant. You know that today is {}",
+            Utc::now().date_naive().format("%Y-%m-%d").to_string()
+        );
+        messages.push(
+            ChatCompletionRequestSystemMessageArgs::default()
+                .content(system_prompt)
+                .build().unwrap()
+                .into());
+        HtmxChat{ socket, messages, env }
+    }
+
     async fn recv(&mut self) -> anyhow::Result<Option<HtmxMessage>> {
         use ProcessError::*;
         match self.socket.recv().await {
@@ -109,6 +126,7 @@ impl HtmxChat {
             Some(Ok(msg)) => bail!(UnknownMessage(msg)),
         }
     }
+
     async fn send(&mut self, msg: Markup) -> anyhow::Result<()> {
         let out_message = html! { div #chat_room class="flex-grow overflow-y-auto p-5 flex flex-col space-y-4" hx-swap-oob="beforeend" { (msg) } };
         self.socket
@@ -116,11 +134,93 @@ impl HtmxChat {
         .await?;
         Ok(())
     }
+
+    async fn send_user(&mut self, msg: &str) -> anyhow::Result<()> {
+        self.messages.push(ChatCompletionRequestUserMessageArgs::default().content(msg).build()?.into());
+        let markup = html! { div class="user-message bg-blue-500 text-white self-end rounded-xl rounded-br-none p-3 max-w-3/4 break-words" { (msg) } };
+        self.send(markup).await?;
+        Ok(())
+    }
+
+    async fn send_assistant(&mut self, msg: &str) -> anyhow::Result<()> {
+        self.messages.push(ChatCompletionRequestAssistantMessageArgs::default().content(msg).build()?.into());
+        let markup = html! { div #assistant class="assistant-message bg-gray-200 text-black self-start rounded-xl rounded-bl-none p-3 max-w-3/4 break-words" { (msg)} };
+        self.send(markup).await?;
+        Ok(())
+    }
+
+    async fn run(&mut self) -> anyhow::Result<()> {
+        loop {
+            match self.recv().await? {
+                Some(msg) => {
+                    self.process_message(&msg.chat_message).await?;
+                }
+                None => return Ok(())
+            }
+        }
+    }
+
+    async fn process_message(&mut self, msg: &str) -> anyhow::Result<()> {
+        self.send_user(&msg).await?;
+
+        loop {
+            let response = self.env.openai_client.chat(&self.messages).await?;
+
+            let mut text_responses: Vec<String> = vec![];
+            let mut tool_calls: Vec<ChatCompletionMessageToolCall> = vec![];
+            for message in response.choices.into_iter().map(|x| x.message) {
+                if let Some(content) = message.content {
+                    text_responses.push(content);
+                }
+                if let Some(tools) = message.tool_calls {
+                    tool_calls.extend(tools);
+                }
+            }
+            let assistant_response = text_responses.join("\n");
+
+            if tool_calls.is_empty() {
+                self.send_assistant(&assistant_response).await?;
+
+                return Ok(())
+            }
+            else {
+                self.process_function_calls(&assistant_response, &tool_calls).await?;
+            }
+
+        }
+    }
+
+    async fn send_tool_calls(&mut self, calls: Vec<ChatCompletionRequestMessage>) -> anyhow::Result<()> {
+        Ok(self.messages.extend(calls))
+    }
+
+    async fn process_function_calls(
+        &mut self,
+        assistant_response: &str,
+        tool_calls: &[ChatCompletionMessageToolCall],
+    ) -> anyhow::Result<()> {
+        let mut messages = vec![];
+        messages.push(ChatCompletionRequestAssistantMessageArgs::default().content(assistant_response).tool_calls(Vec::from(tool_calls)).build()?.into());
+        for call in Vec::from(tool_calls) {
+            debug!("Calling {}", call.function.name);
+            let id = call.id;
+            let response = self.env.mcp.call_tool(&call.function).await?;
+            let mut text_response = String::new();
+            for raw in response.content.into_iter().map(|x| x.raw) {
+                match raw {
+                    RawContent::Text(RawTextContent { text }) => text_response.push_str(&text),
+                    x => bail!("Unknown response: {:?}", x),
+                }
+            }
+            messages.push(ChatCompletionRequestToolMessageArgs::default().content(text_response).tool_call_id(id).build()?.into());
+        }
+        self.send_tool_calls(messages).await?;
+        Ok(())
+    }
 }
 
 #[instrument(skip(socket, env))]
 async fn handle_socket(mut socket: WebSocket, env: Arc<Env>) {
-    let env = env.deref();
     if socket
         .send(Message::Ping(Bytes::from_static(&[1, 2, 3])))
         .await
@@ -133,35 +233,11 @@ async fn handle_socket(mut socket: WebSocket, env: Arc<Env>) {
     }
 
     if let Some(Ok(Message::Pong(_))) = socket.recv().await {
-        let mut chatter = HtmxChat { socket };
-        let mut messages: Vec<ChatCompletionRequestMessage> = Vec::new();
-        let system_prompt = format!(
-            "You are a helpful assistant. You know that today is {}",
-            Utc::now().date_naive().format("%Y-%m-%d").to_string()
-        );
-        messages.push(
-            ChatCompletionRequestSystemMessageArgs::default()
-                .content(system_prompt)
-                .build().unwrap()
-                .into(),
-        );
+        let mut chatter = HtmxChat::new(socket, env);
+
         // Ping success
         // Start the main loop
-        loop {
-            match chatter.recv().await {
-                Ok(Some(msg)) => {
-                    if let Err(err) = process_message(env, &mut chatter, &mut messages, msg).await {
-                        warn!("{:?}", err);
-                        return;
-                    }
-                }
-                Ok(None) => return,
-                Err(err) => {
-                    warn!("{:?}", err);
-                    return;
-                }
-            }
-        }
+        chatter.run().await.expect("Failed");
     }
 }
 
@@ -171,80 +247,4 @@ enum ProcessError {
     UnknownMessage(Message),
 }
 
-async fn process_message(
-    env: &Env,
-    chatter: &mut HtmxChat,
-    messages: &mut Vec<ChatCompletionRequestMessage>,
-    msg: HtmxMessage,
-) -> anyhow::Result<()> {
-    chatter
-        .send(html! { div class="user-message bg-blue-500 text-white self-end rounded-xl rounded-br-none p-3 max-w-3/4 break-words" { (msg.chat_message) } })
-    .await?;
 
-    messages.push(
-        ChatCompletionRequestUserMessageArgs::default()
-            .content(msg.chat_message.clone())
-            .build()?
-            .into(),
-    );
-
-    loop {
-        let response = env.openai_client.chat(&messages).await?;
-
-        let mut text_responses: Vec<String> = vec![];
-        let mut tool_calls: Vec<ChatCompletionMessageToolCall> = vec![];
-        for message in response.choices.into_iter().map(|x| x.message) {
-            if let Some(content) = message.content {
-                text_responses.push(content);
-            }
-            if let Some(tools) = message.tool_calls {
-                tool_calls.extend(tools);
-            }
-        }
-        let assistant_response = text_responses.join("\n");
-
-        if tool_calls.is_empty() {
-            messages.push(
-                ChatCompletionRequestAssistantMessageArgs::default()
-                    .content(assistant_response.clone())
-                    .build()?
-                    .into(),
-            );
-            chatter
-                .send(html! { div #assistant class="assistant-message bg-gray-200 text-black self-start rounded-xl rounded-bl-none p-3 max-w-3/4 break-words" { (assistant_response)} })
-            .await?;
-
-            return Ok(())
-        }
-        else {
-            let new_messages =
-                process_function_calls(&env, &assistant_response, &tool_calls).await?;
-            messages.extend(new_messages);
-        }
-
-    }
-}
-
-async fn process_function_calls(
-    env: &Env,
-    assistant_response: &str,
-    tool_calls: &[ChatCompletionMessageToolCall],
-) -> anyhow::Result<Vec<ChatCompletionRequestMessage>> {
-    let mut messages = vec![];
-    // messages.push(assistant_message(&assistant_response, Some(Vec::from(tool_calls)))?.into());
-    messages.push(ChatCompletionRequestAssistantMessageArgs::default().content(assistant_response).tool_calls(Vec::from(tool_calls)).build()?.into());
-    for call in Vec::from(tool_calls) {
-        debug!("Calling {}", call.function.name);
-        let id = call.id;
-        let response = env.mcp.call_tool(&call.function).await?;
-        let mut text_response = String::new();
-        for raw in response.content.into_iter().map(|x| x.raw) {
-            match raw {
-                RawContent::Text(RawTextContent { text }) => text_response.push_str(&text),
-                x => bail!("Unknown response: {:?}", x),
-            }
-        }
-        messages.push(ChatCompletionRequestToolMessageArgs::default().content(text_response).tool_call_id(id).build()?.into());
-    }
-    Ok(messages)
-}
